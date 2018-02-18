35cecc53e49421d21766766db107f27e5894522c
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-02-18 20:00:44 +0000

    Issue #3: implemented ToolPose tests

--- tests/track/test_tool_pose.py
+++ tests/track/test_tool_pose.py
@@ -4,20 +4,79 @@ from wherepy.track import ToolPose
 
 class ToolPoseTestCase(unittest.TestCase):
 
+    @staticmethod
+    def setUpClass(cls):
+        # valid means not None
+        cls.valid_tid = '1'
+        cls.invalid_tid = None
+
+        # validity means len() = 4 here
+        # no other check is performed
+        cls.valid_quaternion = [0.0, 1.0, 2.0, 3.0]
+        cls.invalid_quaternion = [0.0, 1.0, 2.0]
+
+        # validity means len() = 3 here
+        # no other check is performed
+        cls.valid_coordinates = [0.0, 1.0, 2.0]
+        cls.invalid_coordinates = [0.0, 1.0, 2.0, 3.0]
+
+        cls.valid_quality = 0.50
+        cls.invalid_qualities = [-0.01, 1.01]
+
+        cls.invalid_timestamp = -123456789.123456
+
     def test_id_validity_checked(self):
-        self.__fail()
+        self.assertRaises(
+            ValueError,
+            ToolPose(tid=ToolPoseTestCase.invalid_tid,
+                     quaternion=ToolPoseTestCase.valid_quaternion,
+                     coordinates=ToolPoseTestCase.valid_coordinates,
+                     quality=ToolPoseTestCase.valid_quality,
+                     error=None
+                     )
+        )
 
     def test_quaternion_validity_checked(self):
-        self.__fail()
+        self.assertRaises(
+            ValueError,
+            ToolPose(tid=ToolPoseTestCase.valid_tid,
+                     quaternion=ToolPoseTestCase.invalid_quaternion,
+                     coordinates=ToolPoseTestCase.valid_coordinates,
+                     quality=ToolPoseTestCase.valid_quality,
+                     error=None
+                     )
+        )
 
     def test_coordinates_validity_checked(self):
-        self.__fail()
+        self.assertRaises(
+            ValueError,
+            ToolPose(tid=ToolPoseTestCase.valid_tid,
+                     quaternion=ToolPoseTestCase.valid_quaternion,
+                     coordinates=ToolPoseTestCase.invalid_coordinates,
+                     quality=ToolPoseTestCase.valid_quality,
+                     error=None
+                     )
+        )
 
     def test_quality_validity_checked(self):
-        self.__fail()
+        for invalid_quality in ToolPoseTestCase.invalid_qualities:
+            self.assertRaises(
+                ValueError,
+                ToolPose(tid=ToolPoseTestCase.valid_tid,
+                         quaternion=ToolPoseTestCase.valid_quaternion,
+                         coordinates=ToolPoseTestCase.valid_coordinates,
+                         quality=invalid_quality,
+                         error=None
+                         )
+            )
 
     def test_timestamp_validity_checked(self):
-        self.__fail()
-
-    def __fail(self):
-        self.fail('not implemented')
+        self.assertRaises(
+            ValueError,
+            ToolPose(tid=ToolPoseTestCase.valid_tid,
+                     quaternion=ToolPoseTestCase.valid_quaternion,
+                     coordinates=ToolPoseTestCase.valid_coordinates,
+                     quality=ToolPoseTestCase.valid_qualities[0],
+                     error=None,
+                     timestamp=ToolPoseTestCase.invalid_timestamp)
+        )
