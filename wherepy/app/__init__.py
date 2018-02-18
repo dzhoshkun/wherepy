271e021f35fd3f70264eaaa07517e6eeacd915fd
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-02-18 12:29:26 +0000

    Issue #4: collector app functions now creating a Collector object

--- wherepy/app/__init__.py
+++ wherepy/app/__init__.py
@@ -1,8 +1,11 @@
+from ._collector import Collector
+
+
 def collector_gui():
     """Start tracking data collector in GUI mode."""
-    pass
+    collector = Collector(graphical=True)
 
 
 def collector_cli():
     """Start tracking data collector in CLI mode."""
-    pass
+    collector = Collector(graphical=False)
