9f5bf05cc5af4411caa8a7bfc4bfad2d6739e7c8
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-02-18 21:00:25 +0000

    Issue #2: added failing SessionLog test stubs

---
+++ tests/io/test_session_log.py
@@ -0,0 +1,14 @@
+import unittest
+from wherepy.io import SessionLog
+
+
+class SessionLogTestCase(unittest.TestCase):
+
+    def test_session_file_created(self):
+        self.__fail()
+
+    def test_tool_pose_appended_to_session_file(self):
+        self.__fail()
+
+    def __fail(self):
+        self.fail('not implemented')
