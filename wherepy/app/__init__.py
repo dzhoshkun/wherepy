f87efccc84c8b997a0f7e1d67334de360f4c832e
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-02-18 15:07:05 +0000

    Issue #13: disabled unused-variable for wherepy.app module methods

--- wherepy/app/__init__.py
+++ wherepy/app/__init__.py
@@ -4,9 +4,15 @@ from ._collector import Collector
 
 def collector_gui():
     """Start tracking data collector in GUI mode."""
+
+    # pylint:disable=unused-variable
+
     collector = Collector(graphical=True)
 
 
 def collector_cli():
     """Start tracking data collector in CLI mode."""
+
+    # pylint:disable=unused-variable
+
     collector = Collector(graphical=False)
