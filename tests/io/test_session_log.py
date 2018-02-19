994a003ee9b2a2bcfc8380db1d4e1202479ef217
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-19 08:14:25 +0000

    Issue #2: implemented invalid path and valid path tests of SessionLog

--- tests/io/test_session_log.py
+++ tests/io/test_session_log.py
@@ -1,14 +1,24 @@
 import unittest
+from os.path import (join, isfile)
+from uuid import uuid4
 from wherepy.io import SessionLog
 
 
 class SessionLogTestCase(unittest.TestCase):
 
+    @staticmethod
+    def unique_string(length=6):
+        return uuid4().hex[:length].upper()
+
     def test_invalid_filepath_raises(self):
-        self.__fail()
+        invalid_filepath = join('this', 'should-not', 'exist', 'session.yml')
+        with self.assertRaises(OSError):
+            SessionLog(filepath=invalid_filepath)
 
     def test_session_file_created(self):
-        self.__fail()
+        filepath = 'session-{}.yml'.format(SessionLogTestCase.unique_string())
+        SessionLog(filepath=filepath)
+        self.assertTrue(isfile(filepath))
 
     def test_tool_pose_appended_to_session_file(self):
         self.__fail()
