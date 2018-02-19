f8ee15a51f43dc0990f3bc2c39024552f75e2135
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-19 21:52:16 +0000

    Issue #24: added wherepy.app.run_indicator_cli stub

---
+++ wherepy/app/_indicator.py
@@ -0,0 +1,16 @@
+"""Internal module that keeps the live tracking quality indicator
+functionality.
+
+"""
+
+
+def run_indicator_cli(tracker, update_rate=10):
+    """Run live CLI quality indicator of passed tracker at desired
+    update rate.
+
+    :param tracker: query this tracker
+    :type tracker: Tracker
+    :param update_rate: update rate in Hz
+    :type update_rate: int
+    """
+    pass
