b6d25e4a69aace52322634deea8e67a22a421a46
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 13:38:05 +0000

    Issue #25: added session_log and update_rate params to collect_n_poses_cli

--- wherepy/app/_collector.py
+++ wherepy/app/_collector.py
@@ -1,7 +1,7 @@
 """This internal module keeps elements related to tracking data collection."""
 
 
-def collect_n_poses_cli(tracker, num_poses):
+def collect_n_poses_cli(tracker, num_poses, session_log, update_rate=10):
     """Start passed tracker, collect specified number of poses, and stop it.
 
     :param tracker: no sanity checks are performed on this passed object, it
@@ -10,6 +10,10 @@ def collect_n_poses_cli(tracker, num_poses):
     :param num_poses: number of poses to collect
     :type num_poses: int - must be positive, obviously, no sanity checks
     performed
+    :param session_log: session log for saving captured poses
+    :type session_log: SessionLog
+    :param update_rate: update rate in Hz
+    :type update_rate: int
     """
 
     pass
