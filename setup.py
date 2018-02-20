09778eaa1dd94536fc73fc55d83c83379d6e514a
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 17:41:24 +0000

    Changed version string to 0.1.1.dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import (setup, find_packages)
 
 setup(
     name='WherePy',
-    version='0.1.0',
+    version='0.1.1.dev0',
     description='Sample application to demonstrate real-time tool tracking',
     url='https://github.com/dzhoshkun/wherepy',
     author='Dzhoshkun Ismail Shakir',
