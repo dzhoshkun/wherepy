ec1d1ab0dc0377d7ec775198b4ce68853c7feb4f
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 13:30:47 +0000

    Issue #25: implemented collector_cli function

--- wherepy/app/__init__.py
+++ wherepy/app/__init__.py
@@ -1,14 +1,29 @@
 """Functions that implement the functionality of console scripts."""
+
+from argparse import ArgumentParser
 from wherepy.track.ndi import Tracker
+from wherepy.io import SessionLog
+from ._utils import (check_positive_int_or_raise, check_non_existing_file_or_raise)
 from ._indicator import run_indicator_cli
+from ._collector import collect_n_poses_cli
 
 
 def collector_cli():
-    """Start tracking data collector in CLI mode."""
+    """Collect tracking data in CLI mode."""
 
-    # pylint:disable=unused-variable
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--num-poses', help='Number of poses to capture',
+                        type=check_positive_int_or_raise,
+                        metavar='N', required=True)
+    parser.add_argument('-o', '--session-log', help='Where to save captured poses',
+                        type=check_non_existing_file_or_raise,
+                        metavar='FILE', required=True)
 
-    pass
+    args = parser.parse_args()
+
+    tracker = Tracker()
+    session_log = SessionLog(args.session_log)
+    collect_n_poses_cli(tracker=tracker, num_poses=args.num_poses, session_log=session_log)
 
 
 def indicator_cli():
