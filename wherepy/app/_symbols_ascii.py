753bb3221dac267e4905ba8aad15ef41a6084c3c
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 14:48:02 +0000

    Issue #27: added dictionary for ASCII symbols

--- wherepy/app/_symbols_ascii.py
+++ wherepy/app/_symbols_ascii.py
@@ -1,1 +1,8 @@
 """This internal module keeps the ASCII symbols used for indicator."""
+
+INDICATOR_SYMBOLS = {
+    'connection_status': {
+        'connected': 'OK',
+        'not connected': 'XX',
+    },
+}
