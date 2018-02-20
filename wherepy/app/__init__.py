155ecc006fe4325393fc37efd9ae318dba3db35a
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 13:01:34 +0000

    Issue #25: removed collector_gui function script to avoid confusion until #1 implemented

--- wherepy/app/__init__.py
+++ wherepy/app/__init__.py
@@ -4,14 +4,6 @@ from ._collector import Collector
 from ._indicator import run_indicator_cli
 
 
-def collector_gui():
-    """Start tracking data collector in GUI mode."""
-
-    # pylint:disable=unused-variable
-
-    collector = Collector(graphical=True)
-
-
 def collector_cli():
     """Start tracking data collector in CLI mode."""
 
