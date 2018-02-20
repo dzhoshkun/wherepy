77776193aeb0d42ed9fd497f3e4720d15a26f3c8
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 09:57:14 +0000

    Issue #24: added display_status function to wherepy.app (with Unicode characters)

--- wherepy/app/_indicator.py
+++ wherepy/app/_indicator.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+
 """Internal module that keeps the live tracking quality indicator
 functionality.
 
 """
 
 from time import sleep
+from sys import stdout
 
 
 def compose_indicator(filled, bar, msg=None):
@@ -15,6 +18,62 @@ def compose_indicator(filled, bar, msg=None):
         status = bar * num_bars
         status += ' ' * num_spaces
     return '|{}|'.format(status)
+
+
+def display_status(connected, quality=None, error=None, msg=None):
+    """Display status of tracking data in the following format:
+    | Device [✓] | Signal [=====     ] 54 % | Error  1.54 mm  | info message here.       |
+    | Device [✗] | Signal [          ]  0 % | Error     ~     |
+
+    :param connected: whether a tracking device is connected
+    :type connected: bool
+    :param quality: tracking signal quality indicator
+    :type quality: float between 0.00 and 1.00
+    :param error: tracking error
+    :type error: float (mm)
+    :param msg: an optional info message
+    :type msg: str
+    """
+    status = ''
+    if connected:
+        connection_status = '✓'  # u'\u2713'
+    else:
+        connection_status = '✗'  # u'\u2717'
+    connection_status = 'Device [{}]'.format(connection_status).center(14)
+    status += '|{}'.format(connection_status)
+
+    total_bars = 10
+    quality_bars = 0
+    space_bars = total_bars - quality_bars
+    arrow = ''
+    if quality:
+        quality_bars = int(quality / (1.0 / total_bars))
+        space_bars = total_bars - quality_bars
+        if quality_bars - 1 > 0:
+            quality_bars -= 1
+            arrow = '>'
+    signal_status = '=' * quality_bars
+    signal_status += arrow
+    signal_status += ' ' * space_bars
+    signal_status = 'Signal [{}] {:2d} %'.format(signal_status, int(100 * quality)).center(26)
+    status += '|{}'.format(signal_status)
+
+    error_status = 'NA'
+    if error:
+        if error == float('inf'):
+            error_status = '  ~  '
+        else:
+            error_status = '{:.2f} mm'.format(error)
+    error_status = 'Error {}'.format(error_status.center(9)).center(17)
+    status += '|{}|'.format(error_status)
+
+    if msg:
+        status += ' {} |'.format(msg[:24]).ljust(24)
+    else:
+        status += ' ' * 27
+
+    stdout.write('{}\r'.format(status))
+    stdout.flush()
 
 
 def run_indicator_cli(tracker, update_rate=10):
