da21d1fc7529f7f9326e640245caa302c461044d
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 13:26:54 +0000

    Issue #25: added check_positive_int_or_raise function stub

--- wherepy/app/_utils.py
+++ wherepy/app/_utils.py
@@ -1,1 +1,11 @@
 """Internal module that keeps utils to be used by console scripts."""
+
+
+def check_positive_int_or_raise(value):
+    """Check passed value is a positive integer.
+
+    :return: value if it passes the check
+    :raises ArgumentTypeError: if passed value not positive
+    """
+
+    pass
