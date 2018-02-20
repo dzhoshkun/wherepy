7cf656484b9be7bf35b513232110575074b4aecd
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 13:12:58 +0000

    Issue #25: added collect_n_poses_cli function stub to internal _collector module

--- wherepy/app/_collector.py
+++ wherepy/app/_collector.py
@@ -1,2 +1,15 @@
 """This internal module keeps elements related to tracking data collection."""
 
+
+def collect_n_poses_cli(tracker, num_poses):
+    """Start passed tracker, collect specified number of poses, and stop it.
+
+    :param tracker: no sanity checks are performed on this passed object, it
+    should be ready to start
+    :type tracker: Tracker
+    :param num_poses: number of poses to collect
+    :type num_poses: int - must be positive, obviously, no sanity checks
+    performed
+    """
+
+    pass
