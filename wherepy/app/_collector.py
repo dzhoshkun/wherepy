4f90f2e2ad278159bda55ad23d64b93bfe342eb6
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2018-02-20 13:43:28 +0000

    Issue #25: implemented collect_n_poses_cli function

--- wherepy/app/_collector.py
+++ wherepy/app/_collector.py
@@ -1,5 +1,7 @@
 """This internal module keeps elements related to tracking data collection."""
 
+from time import sleep
+
 
 def collect_n_poses_cli(tracker, num_poses, session_log, update_rate=10):
     """Start passed tracker, collect specified number of poses, and stop it.
@@ -16,4 +18,34 @@ def collect_n_poses_cli(tracker, num_poses, session_log, update_rate=10):
     :type update_rate: int
     """
 
-    pass
+    update_interval = 1.0 / update_rate
+
+    captured = 0
+    for _ in range(3 * num_poses):
+        if not tracker.connected:
+            try:
+                tracker.connect()
+            except IOError:
+                pass
+
+        if tracker.connected:
+            try:
+                tool_pose = tracker.capture(tool_id=1)
+            except IOError:
+                pass
+            except ValueError:
+                pass
+            else:
+                session_log.append(tool_pose)
+                captured += 1
+
+        if captured >= num_poses:
+            break
+
+        sleep(update_interval)
+
+    if tracker.connected:
+        try:
+            tracker.disconnect()
+        except IOError:
+            pass
