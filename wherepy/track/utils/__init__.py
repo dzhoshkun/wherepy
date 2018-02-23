aea70c7e5c71ec494fa1655b3c6d65bbc44bbf21
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-02-23 09:33:30 +0000

    Issue #34: added a wherepy.track.utils.quality function stub

--- wherepy/track/utils/__init__.py
+++ wherepy/track/utils/__init__.py
@@ -1,1 +1,14 @@
 """Utility functions for tracking data processing."""
+
+
+def quality(error, max_error, min_error=0.0):
+    """Compute a quality percentage, based on error.
+
+    :param error:
+    :param max_error: maximum allowable error
+    :param min_error: all smaller values than this yield
+    100 % quality
+    :return: a value btw. 0.00 and 1.00, representing a
+    percentage
+    """
+    return 0.0
